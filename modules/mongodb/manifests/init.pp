7d548624fa9f7d706e613941b5cafa1d7aabecee
Daniel Mabbett <DanielMabbett@Daniels-MacBook-Pro.local>
2016-02-16 11:41:44 +0000

    changes made to MongoDB module[now ubuntu] - DM

--- modules/mongodb/manifests/init.pp
+++ modules/mongodb/manifests/init.pp
@@ -1,38 +1,37 @@
 class mongodb {
    
-   Exec {
+  Exec {
     path => [
-	  '/usr/local/bin',
-	  '/opt/local/bin',
-	  '/usr/bin',
-	  '/bin',
-	  '/sbin'],
-	logoutput => true,
-	timeout => 0,
+      '/usr/local/bin',
+      '/opt/local/bin',
+      '/usr/bin',
+      '/bin',
+      '/sbin'],
+    logoutput => true,
+    timeout => 0,
   }
   
-  file {'/etc/yum.repos.d/mongodb.repo':
-    ensure => present,
-	source => 'puppet:///modules/mongodb/mongodb.repo',
-	owner  => root,
+  exec {'import_mongodb_public_gpg_key':
+    command => 'sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10',
+    user    => root,
+    before  => Exec['create_mongodb_source_list'],
   }
   
-  exec {'install_mongodb':
-    command => 'yum install -y mongodb-org',
-	require => File['/etc/yum.repos.d/mongodb.repo'],
-	user    => root,
-	before  => Exec['restart_mongod'],
+  exec {'create_mongodb_source_list':
+    command => 'echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list',
+    user    => root,
+    before  => Exec['update_package_database_mongodb'],
   }
-  
-  exec {'restart_mongod':
-    command => '/etc/init.d/mongod restart',
-	user    => root,
-	before  => Exec['auto_mongod'],
+   
+  exec {'update_package_database_mongodb':
+    command => 'sudo apt-get update',  
+    user    => root
+    before  => Exec['install_stable_mongodb'],
   }
-  
-  exec {'auto_mongod':
-    command => 'chkconfig mongod on',
-	user    => root,
+
+  exec {'install_stable_mongodb':
+    command => 'sudo apt-get install mongodb-org',
+    user    => root,
   }
 
 }
